fbb129a444baa950cc34132b1ea73e593f233d3d
Tanyuha <tani.tkalenko@yandex.ua>
2016-08-29 15:38:02 +0300

    perfect-pixel

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
                 '<p class="click">This item has already been added to your list</p></div>');
 
             image.children().addClass('img-tr');
-            $('.present-list').append('<div class="bl-one" data-index="' + $(this).index() + '">' +
+            $('.present-list').append('<div class="bl-one" data-index="' + $(this).index() + '">'+
                 '<div class="remove">' +
                 '<div class="view-item btn2"><span><i class="fa fa-search" style="font-size:20px"></i>View item</span></div>' +
                 '<div class="more-options btn2"><div>More Options<i class="fa fa-caret-down" style="font-size:20px" aria-hidden="true"></i></div>' +
@@ -33,6 +33,7 @@ $(document).ready(function () {
                 '<p class="cap2">Best Price: &pound;12</p>' +
                 '<p class="cap3">By: <img src="img/amazon.png"></p>' +
                 '</div>' +
+                '<img class="clip-two" src="img/clip-two.png" alt="">' +
                 '</div>'
             );
             $(this).addClass('cloned');
@@ -59,6 +60,37 @@ $(document).ready(function () {
     $('#nav-toggle').on('click', function () {
         $('.header-nav').stop().slideToggle();
         $(this).toggleClass('on')
+    });
+
+
+
+    // Configure/customize these variables.
+    var showChar = 125;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Read more &raquo;";
+    var lesstext = "Read less &raquo;";
+
+    $('.more').each(function() {
+        var content = $(this).html();
+        if(content.length > showChar) {
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>'
+                + h + '</span>&nbsp;&nbsp;<p class="morelink link">' + moretext + '</p></span>';
+            $(this).html(html);
+        }
+    });
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
     });
 
 
